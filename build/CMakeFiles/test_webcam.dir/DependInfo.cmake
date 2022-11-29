688493d760f20b3bcf5096e21b9f5759a81c551d
peng liu <peng.liu@matrx.team>
2022-11-29 14:54:29 +0400

    first commit

---
+++ build/CMakeFiles/test_webcam.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/Background.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/Background.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/BackgroundMatting.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMatting.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/BackgroundMattingConfig.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMattingConfig.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/BackgroundMattingMNN.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMattingMNN.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/Profiler.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/Profiler.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/background_matting/utils.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/background_matting/utils.cpp.o"
+  "/home/pengliu/Desktop/backgroundmattingcpponnx/src/test_webcam.cpp" "/home/pengliu/Desktop/backgroundmattingcpponnx/build/CMakeFiles/test_webcam.dir/src/test_webcam.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_webcam.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_webcam.dir/src/background_matting/Background.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMatting.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMattingConfig.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/background_matting/BackgroundMattingMNN.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/background_matting/Profiler.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/background_matting/utils.cpp.o"
+  "CMakeFiles/test_webcam.dir/src/test_webcam.cpp.o"
+  "test_webcam"
+  "test_webcam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_webcam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
